216a3dd064ce35322ffe3422eb24f386d3de9045
Anmicius <allenlin@disroot.org>
2020-07-15 09:27:11 +0800

    propaganda!

--- src/components/tools/Propaganda/Propaganda.js
+++ src/components/tools/Propaganda/Propaganda.js
@@ -1,25 +1,37 @@
 import React, { useEffect, useState } from "react"
 import { LazyLoadImage } from "react-lazy-load-image-component"
 import Jumbotron from "react-bootstrap/Jumbotron"
+import Swiper from "react-id-swiper"
 
 // style
 import "./Propaganda.scss"
 
 const Propaganda = () => {
+  // get image
   const [image, setImage] = useState()
-
   useEffect(() => {
-    fetch(
-      `https://wordpress.hsnu.org/index.php/wp-json/wp/v2/propaganda?filter[orderby]=rand&filter[posts_per_page]=1`
-    )
-      .then(res => {
-        return res.json()
-      })
-      .then(data => {
-        setImage(data)
-        console.log(image)
-      })
-  }, [])
+    setTimeout(() => {
+      fetch(
+        `https://wordpress.hsnu.org/wp-json/wp/v2/propaganda?filter[orderby]=rand&filter[posts_per_page]=1`
+      )
+        .then(res => {
+          return res.json()
+        })
+        .then(data => {
+          setImage(data)
+          console.log(data)
+        })
+    }, 5000)
+  }, [image])
+
+  // Swiper config
+  const params = {
+    loop: "true",
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  }
 
   return (
     <Jumbotron id="propaganda">
