3551486ea41a3960fc51c72f0d12e383a8521694
Anmicius <allenlin@disroot.org>
2020-08-20 21:32:45 +0800

    simplify wordpress setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `國立臺灣師範大學附屬高級中學`,
     description: `國立臺灣師範大學附屬高級中學，簡稱台師大附中、師大附中、附中，官方英文簡稱為HSNU，是一所位處台北市中心、位於臺北市大安區信義路上的高級中學，附屬於國立臺灣師範大學。`,
-    siteUrl: "https://hsnu.org", // No trailing slash allowed!
+    siteUrl: "https://hsnu.org",
     image: "https://i.ytimg.com/vi/2fb-g_V-UT4/hqdefault.jpg",
   },
   plugins: [
@@ -11,18 +11,18 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-transformer-json`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `師大附中`,
-        short_name: `HSNU`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#ffffff`,
-        display: `standalone`,
-        icon: `src/images/icons/HSNU.png`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `師大附中`,
+    //     short_name: `HSNU`,
+    //     start_url: `/`,
+    //     background_color: `#ffffff`,
+    //     theme_color: `#ffffff`,
+    //     display: `standalone`,
+    //     icon: `src/images/icons/HSNU.png`,
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-layout`,
       options: {
@@ -40,32 +40,6 @@ module.exports = {
         hostingWPCOM: false,
         // does your site use the Advanced Custom Fields Plugin?
         useACF: true,
-        // Blacklisted routes using glob patterns
-        excludedRoutes: [
-          // acf
-          "**/acf/v3/posts",
-          "**/acf/v3/pages",
-          "**/acf/v3/categories",
-          "**/acf/v3/tags",
-          "**/acf/v3/comments",
-          "**/acf/v3/blocks",
-          "**/acf/v3/users",
-          // wordpress
-          "**/wp/v2/blocks",
-          "**/wp/v2/statuses",
-          "**/wp/v2/taxonomies",
-          "**/wp/v2/categories",
-          "**/wp/v2/tags",
-          "**/wp/v2/posts",
-          "**/wp/v2/pages",
-          "**/wp/v2/types",
-          "**/wp/v2/users",
-          "**/wp/v2/settings",
-          "**/wp/v2/themes",
-          // jetpack
-          "**/jetpack/**",
-          "**/wpcom/**",
-        ],
       },
     },
     // canonical link
