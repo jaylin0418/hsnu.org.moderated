632faaefa9bf6d7f9f40d672c607d4e11d5161eb
Anmicius <allenlin@disroot.org>
2020-08-20 21:32:12 +0800

    merge top.js and preview.js to review.js

--- src/pages/preview.js
+++ src/pages/preview.js
@@ -13,17 +13,27 @@ import Content from "../components/tools/Content/Content"
 import RecommandVideo from "../components/tools/RecommandVideo/RecommandVideo"
 
 export default () => {
+  // get post type e.g: spost, top
+  const post_type =
+    typeof document !== "undefined"
+      ? new URL(document.URL).searchParams.get("post_type")
+      : ""
+
+  // get post id
   const wordpress_id =
     typeof document !== "undefined"
       ? new URL(document.URL).searchParams.get("id")
       : ""
+
+  // set post content and status
   const [post, setPost] = useState()
   const [status, setStatus] = useState("Loading...")
 
+  // fetch post
   useEffect(() => {
     axios
       .get(
-        `https://wordpress.hsnu.org/index.php/wp-json/wp/v2/spost?include=${wordpress_id}`
+        `https://wordpress.hsnu.org/index.php/wp-json/wp/v2/${post_type}?include=${wordpress_id}`
       )
       .then(res => {
         console.log(res.data[0])
@@ -34,6 +44,7 @@ export default () => {
           genre: res.data[0].acf.genre,
           date: res.data[0].date,
           urls: res.data[0].acf.repeater_link,
+          files: res.data[0].acf.repeater_file,
           office: res.data[0].acf.last_name,
           section: res.data[0].acf.first_name,
         })
@@ -45,11 +56,7 @@ export default () => {
 
   return (
     <>
-      {post ? (
-        <Seo title={post.title} description={post.content} />
-      ) : (
-        <Seo article />
-      )}
+      {post ? <Seo title={post.title} description={post.content} /> : <Seo />}
 
       <div id="header-padding" />
       <Container id="article" fluid>
@@ -62,6 +69,7 @@ export default () => {
                 date={post.date}
                 content={post.content}
                 urls={post.urls}
+                files={post.files}
                 office={post.office}
                 section={post.section}
               />
