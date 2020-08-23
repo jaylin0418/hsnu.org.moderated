ef336796205f286b355f58f1b17235360f6c7541
jaylin0418 <57249638+jaylin0418@users.noreply.github.com>
2020-08-23 11:30:40 +0800

    Update RecommandVideo.js

--- src/components/tools/RecommandVideo/RecommandVideo.js
+++ src/components/tools/RecommandVideo/RecommandVideo.js
@@ -20,7 +20,7 @@ const RecommandVideo = () => {
         height="315"
         src="https://www.youtube-nocookie.com/embed/w0mog_xxY9Q"
         frameborder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
       ></iframe>
     </Sticky>
